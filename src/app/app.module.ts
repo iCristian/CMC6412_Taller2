36b596be53fc261f027e4a9533296e6dbfeb40a3
iCristian <icristian.mac@gmail.com>
2017-10-30 14:12:03 -0300

    Clase 30 OCT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,40 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { HttpModule } from '@angular/http';
+
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyC7K6MzjNxre7Wf3NHgZYeUtlS6mN0tyCw",
+  authDomain: "cmc6412-23f92.firebaseapp.com",
+  databaseURL: "https://cmc6412-23f92.firebaseio.com",
+  projectId: "cmc6412-23f92",
+  storageBucket: "cmc6412-23f92.appspot.com",
+  messagingSenderId: "815069036535"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
   ],
   providers: [
     StatusBar,
