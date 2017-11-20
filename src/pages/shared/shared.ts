dda04a7179daa2b79625ad5a86eee23dc0708b6f
iCristian <icristian.mac@gmail.com>
2017-11-20 09:56:08 -0300

    20/11

--- src/pages/shared/shared.ts
+++ src/pages/shared/shared.ts
@@ -1,25 +1,60 @@
+import { Observable } from 'rxjs/Observable';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SharedPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, /*NavParams,*/ AlertController, ToastController } from 'ionic-angular';
+import { FirebaseService } from '../../providers/firebase-service/firebase-service';
 
 @IonicPage()
 @Component({
-  selector: 'page-shared',
+  selector: 'page-shared-page',
   templateUrl: 'shared.html',
 })
 export class SharedPage {
+  sharedLists : Observable<any[]>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public firebaseService: FirebaseService, public alertCtrl: AlertController, public toastCtrl: ToastController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SharedPage');
+  ionViewWillEnter() {
+    this.firebaseService.authState.subscribe(user => {
+      if (user) {
+        this.sharedLists = this.firebaseService.getSharedLists();
+      }
+    })
+  }
+
+  addItemToList(listId, listName) {
+    let prompt = this.alertCtrl.create({
+      title: 'New item for "' + listName + '"',
+      message: 'Enter a name for your new item',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Milk'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Add Item',
+          handler: data => {
+            this.firebaseService.addListItem(listId, data.name).then(data => {
+              this.presentToast('New item added!');
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
